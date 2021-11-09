21a41f97a4cb372941359ef845620215ce3d3140
Toshinori Sumida <t.sumida1987@gmail.com>
2021-11-10 02:40:34 +0900

    setUp() セットアップ 初期データ作成

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,14 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    public $data;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->data = 'xxx';
+    }
+
     /**
      * A basic test example.
      *
@@ -14,8 +22,12 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
+        // 準備
+
+        // 実行
         $response = $this->get('/');
 
+        // 検証
         $response->assertStatus(200);
     }
 }
